13c1ee394f88d73bc9454026d523b1818235bbf7
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2022-08-03 15:44:18 +0530

    Disney + Hotstar Clone Project
    This is the entire file that is used to build the clone of Disney + Hotstar project.

---
+++ Disney + Hotstar Clone/index.js
@@ -0,0 +1,33 @@
+let movies = [
+  {
+    name: "Spider-Man 3",
+    des:
+      "New York City is in the throes of Spider-mania! But when a strange alien symbiote turns Spider-Man's suit black, his darkest demons come to light.",
+    image: "images/slider 1.webp"
+  },
+  {
+    name: "Masoom",
+    des:
+      "In the pursuit of justice for her deceased mother, a fierce Sana Kapoor fights her father and uncovers secrets that can impair her relationship with her family.",
+    image: "images/slider 2.webp"
+  },
+  {
+    name: "Jatt & Juliet 2",
+    des:
+      "A police constable from Punjab lands in Canada in search of the deputy commissioner's daughter. Things get messy when he falls in love with her.",
+    image: "images/slider 3.webp"
+  },
+  {
+    name: "Baymax!",
+    des:
+      "The affable, inflatable healthcare companion robot sets out to do what he does best: help others",
+    image: "images/slider 4.webp"
+  },
+  {
+    name: "The Amazing Spider-Man",
+    des:
+      "A teenage Peter Parker grapples with both high school and amazing super-human crises as his alter-ego Spider-Man.",
+    image: "images/slider 5.webp"
+  }
+  ];
+  
